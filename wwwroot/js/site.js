041d4fec38fefae0c995b4359c394c84b5e21916
Fernandez Castellano <ruben.fernandezcastellano@unisys.com>
2023-02-20 13:11:29 +0100

    correccion errores

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -20,10 +20,4 @@ let label_toggle=document.getElementById("label_toggle")
 toggle.addEventListener('change', (event) => {
     let checked = event.target.checked;
     document.body.classList.toggle('dark');
-    if (checked == true) {
-        label_toggle.innerHTML = <i class="fa-solid fa-sun"></i>
-    } else {
-        label_toggle.innerHTML = <i class="fa-solid fa-moon"></i>
-    }
-    
-})
+});
